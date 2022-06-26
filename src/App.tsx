35e89a9c2413ef6a1fc71d889e5b371529140b2c
Sebastian Lundqvist <sebastian.lundqvist@systecongroup.com>
2022-06-26 09:58:16 +0200

    Center board

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import Box from '@mui/material/Box';
 import './App.css';
 import Board from './Board/Board';
 
 const App = () => (
 	<div className="App">
-		<Board />
+		<Box
+			alignItems="center"
+			display="flex"
+			justifyContent="center"
+			minHeight="100vh"
+		>
+			<Board />
+		</Box>
 	</div>
 );
 
